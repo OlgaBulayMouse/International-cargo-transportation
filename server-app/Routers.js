88ffe1f46ba277a6fbc141790b34720fecbf4acf
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2022-01-15 18:53:54 +0300

    changing the route search function

--- server-app/Routers.js
+++ server-app/Routers.js
@@ -1,9 +1,23 @@
 'use strict';
 
-const ROUTER = ['Minsk', 'Fanipol', 'Dzerzhinsk',
- 'Stolbtsy', 'Mir','Baranovichi', 'Ivatsevichi', 
- 'Bereza','Kobrin', 'Brest', 'Biala Podlaska','Siedlce', 
- 'Warsaw', 'Lodz','Wroclaw', 'Legnica', 'Görlitz','Bautzen', 
- 'Dresden', 'Leipzig','Dortmund', 'Dusseldorf', 'Eindhoven', 'Rotterdam' ]
-
- module.exports = ROUTER;
+const ROUTER = [
+    [   'Minsk', 'Fanipol', 'Dzerzhinsk',
+        'Stolbtsy', 'Mir', 'Baranovichi', 'Ivatsevichi',
+        'Bereza', 'Kobrin', 'Brest', 'Biala Podlaska', 'Siedlce',
+        'Warsaw', 'Lodz', 'Wroclaw', 'Legnica', 'Görlitz', 'Bautzen',
+        'Dresden', 'Leipzig', 'Dortmund', 'Dusseldorf', 'Eindhoven', 'Rotterdam'
+    ],
+    [
+        'Paris', 'Reims', 'Kuven', 'Namur', 'Brussels', 'Liege', 'Koln', 'Bayreuth',
+        'Karlovy Vary', 'Prague', 'Brno', 'Zakopane', 'Krakow', 'Tarnow', 'Lviv',
+        'Chervonograd', 'Novovolynsk', 'Kovel', 'Brest', 'Kobrin', 'Ivanovo', 'Pinsk',
+        'Mozyr', 'Rechitsa', 'Gomel', 'Klintsy', 'Bryansk', 'Kaluga', 'Obninsk',
+        'Naro-Fominsk', 'Moscow',
+    ],
+    [
+        'Grodno', 'Bialystok', 'Warsaw', 'Krakow', 'Trencin', 'Bratislava', 
+        'Wiener Neustadt', 'Leoben', 'Liezen', 'Schladming', 'Salzburg', 'Innsbrug', 
+        'Bolzano', 'Trento', 'Verona', 'Brescia', 'Milan', 'Genoa', 
+    ],
+]
+module.exports = ROUTER;
--- server-app/searchCity.js
+++ server-app/searchCity.js
@@ -1,11 +1,15 @@
 'use strict';
 
 function searchCity(city, arr) {
-    for (let i = 0; i <= arr.length; i++) {
-        if (arr[i] === city) {
-            return 'Hello, mister ! We are ready to cooperate with you!'
-        };
-    } return 'Sorry, unfortunately we are not on our way. We will be glad to cooperate next time.';
+    for (let n = 0; n <= arr.length; n++) {
+        if (n <= arr.length - 1) {
+            for (let i = 0; i <= arr[n].length; n[i++]) {
+                if (arr[n][i] === city) {
+                    return 'Hello, mister ! We are ready to cooperate with you!'
+                };
+            };
+        } else return 'Sorry, unfortunately we are not on our way. We will be glad to cooperate next time';
+    };
 };
 
 module.exports = searchCity;
