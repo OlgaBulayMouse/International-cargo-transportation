4f48c67500d14672677a7320d51ec255fec9af94
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2022-01-24 18:07:04 +0300

    Sending a message to the users email address.

--- front-app/src/components/form.jsx
+++ front-app/src/components/form.jsx
@@ -1,31 +1,35 @@
 import { useState } from 'react';
 import './style.css';
+
 export const Form = () => {
+    const [formDate, setFormDate] = useState({
+        fullname: '',
+        mail: '',
+        telephone: '',
+        escort: 'yes',
+        city: 'Grodno',
+        citys: '',
+    });
+
     const handlerChange = event => {
         setFormDate({
             ...formDate,
             [event.target.name]: event.target.value,
         })
     };
-    const [formDate, setFormDate] = useState({
-        fullname: '',
-        mail: '',
-        telephone: '',
-        escort: 'yes',
 
-    });
     const handlerFormDate = event => {
         event.preventDefault();
         fetch('http://localhost:2022', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: 'JSON.stringify(formDate)'
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(formDate),
         })
-        .then(res => console.log(res))
-        .catch(error => console.log(error));
-        console.log(formDate);
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
     };
 
     return (
@@ -67,17 +71,18 @@ export const Form = () => {
                 </p>
             </fieldset>
 
-
             <fieldset className='fieldset'>
                 <legend style={{ color: '#006633' }}>Desired Route</legend>
                 <p>
                     <label htmlFor={'city'}>
                         Place of shipment
-                        <em style={{ color: 'red' }}>*</em>
                     </label>
                     <input style={{ backgroundColor: 'rgb(211, 241, 231)' }}
+                        name={'city'}
                         type={'text'}
-                        id={'city'}>
+                        id={'city'}
+                        value={'Grodno'}
+                        onChange={handlerChange}>
                     </input>
                 </p>
                 <p>
@@ -86,8 +91,11 @@ export const Form = () => {
                         <em style={{ color: 'red' }}>*</em>
                     </label>
                     <input style={{ backgroundColor: 'rgb(211, 241, 231)' }}
+                        name='citys'
                         type={'text'}
-                        id={'citys'}>
+                        id={'citys'}
+                        value={formDate.citys}
+                        onChange={handlerChange}>
                     </input>
                 </p>
                 <p>
@@ -96,15 +104,16 @@ export const Form = () => {
                         name={'escort'}
                         value={'yes'}
                         id={'connection'}
-                        onChange={handlerChange}/>
+                        onChange={handlerChange} />
                 </p>
                 <p>
                     <label htmlFor={'conn'}>Cargo does not need escort</label>
-                    <input type={'radio'} 
+                    <input type={'radio'}
                         name={'escort'}
                         value={'no'}
                         id={'conn'}
-                        onChange={handlerChange}/>
+                        onChange={handlerChange}
+                    />
                 </p>
                 <p>
                     <input type={'submit'}
