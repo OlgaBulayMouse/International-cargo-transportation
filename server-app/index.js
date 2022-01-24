4325f50c72e61050d16717eb2927071418d8b78c
OlgaMenkoMouse <90971820+OlgaMenkoMouse@users.noreply.github.com>
2022-01-24 17:38:54 +0300

    Sending a letter with the text of the message about the possibility / impossibility of cooperation to the user entered

--- server-app/index.js
+++ server-app/index.js
@@ -8,7 +8,6 @@ const searchCity = require('./searchCity.js');
 const ROUTER = require('./Routers');
 const nodemailer = require('nodemailer');
 
-
 app.use(cors({
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200,
@@ -16,43 +15,42 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.yandex.ru',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'olga281205',
-        pass: 'WT)Jz$2KgsHsvga',
-    },
-});
-
-function sendEmail() {
-    const mailOptions = {
-        from: 'olga281205@yandex.ru',
-        to: "olga.menko.84@gmail.com",
-        subject: 'Message from server',
-        text: "Hello, Olga!",
-    };
-
-    transporter.sendMail(mailOptions, () => {
-        if ('yes!' || 'no!') {
-            console.log('Email sent!');
-        } else console.log('No send!');
-    });
-};
-
 app.post('/', (request, respons) => {
     let resultSearch = searchCity(request.body.citys, ROUTER);
     const nameCustomer = request.body.fullname;
-    //const mailAddress = request.body.mail;
+
     if (resultSearch === 'yes!') {
         resultSearch = `Hello, mister (madam) ${nameCustomer}! We are glad to cooperate with you. We will contact you shortly to clarify the details of the order.`;
     } else resultSearch = `Sorry, mister (madam) ${nameCustomer}! The terms of your order do not suit us. We look forward to working with you next time.`
 
-    sendEmail();
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.yandex.ru',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'olga281205',
+            pass: 'WT)Jz$2KgsHsvga',
+        },
+    });
+    
+    function sendEmail(info) {
+        const mailOptions = {
+            from: 'olga281205@yandex.ru',
+            to: request.body.mail,
+            subject: 'Message from carrier',
+            text: resultSearch,
+        };
+    
+        transporter.sendMail(mailOptions, () => {
+            if (info) {
+                console.log(`Email sent to ${request.body.mail}: ${info}`);
+            } else console.log('No send!');
+        });
+    };
+
+    sendEmail(resultSearch);
 
     console.log(request.body);
     respons.json(resultSearch);
 });
-
 app.listen(2022);
